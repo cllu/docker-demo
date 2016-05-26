1c529b4cfaa107998aecc7b323d1386c874f8204
Chunliang Lyu <hi@chunlianglyu.com>
2016-05-26 15:45:26 +0800

    Initial commit

---
+++ demo/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import redis
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "welcome"})
+
+
+@app.route('/reserve')
+def reserve():
+    r = redis.StrictRedis(host='redis', port=6379, db=0)
+    inventory = r.decr('inventory', 1)
+    return jsonify({"inventory": inventory})
+
+
+@app.route('/reset')
+def reset():
+    r = redis.StrictRedis(host='redis', port=6379, db=0)
+    return jsonify({"result": r.set('inventory', 500000)})
